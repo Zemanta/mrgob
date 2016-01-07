282a0d034e1b2a76dcb82e51e7300d8c025807a3
Jure Ham <jure@hamsworld.net>
2016-01-07 12:38:32 +0100

    Changed config env parameter

--- job/config.go
+++ job/config.go
@@ -10,7 +10,7 @@ var ErrMissingJobConfig = fmt.Errorf("Missing job config")
 
 // Config retrieves and decodes the job config passed from the runner.
 func Config(target interface{}) error {
-	cstr := os.Getenv("mrgob.config")
+	cstr := os.Getenv("mrgob_config")
 	if cstr == "" {
 		return ErrMissingJobConfig
 	}
