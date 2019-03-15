9dc5410546a128d179fcae3b490f4f65e25e1ce4
Matic Žgur <mr.zgur@gmail.com>
2019-03-15 15:01:34 +0100

    job: test helpers (uncommited changes).

--- job/counter.go
+++ job/counter.go
@@ -6,10 +6,14 @@ import (
 )
 
 var AppCounterGroup = "GOMR"
+var CounterPipe = os.Stderr
 
 var counterMsg = "reporter:counter:%s,%s,%d\n"
 
 // Count increases hadoop counter for the running job. Use it for counting processed lines, errors etc.
 func Count(name string, c int) {
-	fmt.Fprintf(os.Stderr, counterMsg, AppCounterGroup, name, c)
+	if CounterPipe == nil {
+		return
+	}
+	fmt.Fprintf(CounterPipe, counterMsg, AppCounterGroup, name, c)
 }
--- job/logger.go
+++ job/logger.go
@@ -1,11 +1,30 @@
 package job
 
 import (
+	"io"
 	"log"
 	"os"
 )
 
 var MRLogPrefix = "[GOMR]"
 
+type Logger interface {
+	Fatal(v ...interface{})
+	Fatalf(format string, v ...interface{})
+	Fatalln(v ...interface{})
+	Flags() int
+	Output(calldepth int, s string) error
+	Panic(v ...interface{})
+	Panicf(format string, v ...interface{})
+	Panicln(v ...interface{})
+	Prefix() string
+	Print(v ...interface{})
+	Printf(format string, v ...interface{})
+	Println(v ...interface{})
+	SetFlags(flag int)
+	SetOutput(w io.Writer)
+	SetPrefix(prefix string)
+}
+
 // Log is an instance of go's logger which prefixes all log lines so they can be collected in the runner.
-var Log = log.New(os.Stderr, MRLogPrefix, log.Ldate|log.Ltime|log.Lshortfile)
+var Log Logger = log.New(os.Stderr, MRLogPrefix, log.Ldate|log.Ltime|log.Lshortfile)
